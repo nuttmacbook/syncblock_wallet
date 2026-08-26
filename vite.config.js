a44e9791826716bbbd950c536e3916de34f46370
nutt <nd7v2gmp8t@privaterelay.appleid.com>
2026-08-27 03:02:32 +0700

    PWA Add

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,34 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
     plugins: [
-        tailwindcss()
+        tailwindcss(),
+        VitePWA({
+        registerType: 'autoUpdate',
+        devOptions: { enabled: true },
+        manifest: {
+            name: 'Syncblock Wallet',
+            short_name: 'Syncblock',
+            start_url: '/',
+            display: 'standalone',
+            background_color: '#ffffff',
+            theme_color: '#1e3a8a',
+            icons: [
+            {
+                src: '/logo.png',
+                sizes: '192x192',
+                type: 'image/png'
+            },
+            {
+                src: '/logo.png',
+                sizes: '512x512',
+                type: 'image/png'
+            }
+            ]
+        }
+        })
     ],
     build: {
         rollupOptions: {
